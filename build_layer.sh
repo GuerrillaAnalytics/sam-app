f1ddd41d13d5a0e11f92f1794b132fc5ace9c639
enda <nope@nope.co.uk>
2019-09-21 21:50:49 +0100

    got lambda reference working, sort of

--- build_layer.sh
+++ build_layer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-pip install -r data-science-layer/requirements.txt -t .aws-sam//build/python/ --upgrade
-zip -r9 .aws-sam/build/python.zip .aws-sam/build/python/
-aws lambda publish-layer-version --layer-name data-science-layer --description "test layer for data science" --zip-file fileb://.aws-sam/build/python.zip --compatible-runtimes python3.7 --profile personal
+pip install -r data-science-layer/requirements.txt -t .aws-sam/layers/python/ --upgrade
+zip -r9 .aws-sam/layers/python/python.zip .aws-sam/layers/python/
+#aws lambda publish-layer-version --layer-name data-science-layer --description "test layer for data science" --zip-file fileb://.aws-sam/build/python.zip --compatible-runtimes python3.7 --profile personal
