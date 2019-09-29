1654a88fcbf2a4b7a6a558221b5f2069ba03e70b
enda <nope@nope.co.uk>
2019-09-29 15:20:06 +0100

    commented out zipping as it caused confusion

--- scripts/build-layer.sh
+++ scripts/build-layer.sh
@@ -15,8 +15,10 @@ mkdir -p $BUILD_DIR
 echo 'Installing requirements into correct lambda path'
 pip install -r data-science-layer/requirements.txt -t $BUILD_DIR
 
+# This is how you zip up a layer if deploying manually
+# Having a Zip in your path confuses SAM though
 #echo 'Zipping everything under the python folder inclusive'
-cd data-science-layer
-rm -f data-science-layer.zip
-zip -r9 data-science-layer.zip .
+#cd data-science-layer
+#rm -f data-science-layer.zip
+#zip -r9 data-science-layer.zip .
 #aws lambda publish-layer-version --layer-name data-science-layer --description "test layer for data science" --zip-file fileb://.aws-sam/build/python.zip --compatible-runtimes python3.7 --profile personal
