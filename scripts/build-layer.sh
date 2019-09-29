21c8edc676e70e53c15753b316986d9efa0820a4
enda <nope@nope.co.uk>
2019-09-29 14:37:27 +0100

    working locally but no module error when deployed

--- scripts/build-layer.sh
+++ scripts/build-layer.sh
@@ -16,7 +16,7 @@ echo 'Installing requirements into correct lambda path'
 pip install -r data-science-layer/requirements.txt -t $BUILD_DIR
 
 #echo 'Zipping everything under the python folder inclusive'
-#cd build
-#rm -f data-science-layer.zip
-#zip -r9 data-science-layer.zip .
+cd data-science-layer
+rm -f data-science-layer.zip
+zip -r9 data-science-layer.zip .
 #aws lambda publish-layer-version --layer-name data-science-layer --description "test layer for data science" --zip-file fileb://.aws-sam/build/python.zip --compatible-runtimes python3.7 --profile personal
