a4d5b1fbdb88c1e027794a51157e8d7e85a177f4
enda <nope@nope.co.uk>
2019-11-02 16:09:22 +0000

    added retaining bucket to deletion script

--- scripts/delete-stack.sh
+++ scripts/delete-stack.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 set -x
-aws cloudformation delete-stack --stack-name aws-sam-getting-started
+
+# Retaining the non-empty bucket while deleting the stack
+aws cloudformation delete-stack --stack-name aws-sam-getting-started --retain-resources MyBucket
